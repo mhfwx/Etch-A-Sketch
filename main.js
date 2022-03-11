99874f5b7593fc53f568e5059fcba0d2c7fc10ba
mhfwx <mahaofei001@gmail.com>
2022-03-10 22:20:34 -0500

    complete basic requirement

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 const container = document.getElementById("container");
 
-function makeRows(rows, cols) {
+function makeRows(sqaures) {
   
-  container.style.gridTemplateColumns=`repeat(${cols},1fr)`;
-  container.style.gridTemplateRows=`repeat(${cols},1fr)`;
-  for (c = 0; c < (rows * cols); c++) {
+  container.style.gridTemplateColumns=`repeat(${sqaures},1fr)`;
+  container.style.gridTemplateRows=`repeat(${sqaures},1fr)`;
+  for (c = 0; c < (sqaures * sqaures); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    cell.style.width='50px';
-    cell.style.height='50px';
+    cell.style.width='10px';
+    cell.style.height='10px';
     container.appendChild(cell).className = "grid-item";
   };
 };
 
-makeRows(5, 5);
+makeRows(64);
+
+gridItem = document.querySelectorAll('.grid-item');
+for (let i=0;i<gridItem.length;i++){
+  gridItem[i].addEventListener('mouseover',(e)=>{
+    e.target.style.backgroundColor='red';
+    
+  })
+}
+
+document.querySelector('#clear').addEventListener('click',()=>{
+
+for (let i=0;i<gridItem.length;i++){
+  gridItem[i].style.backgroundColor='white';
+    
+}
+})
